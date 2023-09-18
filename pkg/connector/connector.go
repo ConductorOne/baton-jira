b8d03b79be4409126028f25183de895002a93950
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2023-09-18 15:12:41 +0200

    feat: jira basic auth connector builder

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -1,49 +1,42 @@
 package connector
 
 import (
-	"context"
-	"errors"
-
 	jira "github.com/andygrunwald/go-jira/v2/cloud"
 )
 
-// TODO: implement your connector here
-type Jira struct {
-	client *jira.Client
-}
-
-type JiraOAuthOptions struct {
-	PrivateKeyPath  string
-	ConsumerKeyPath string
-}
-
-type JiraBasicAuthOptions struct {
-	Username string
-	ApiKey   string
-}
-
-type JiraOptions struct {
-	Url       string
-	BasicAuth *JiraBasicAuthOptions
-	OAuth     *JiraOAuthOptions
-}
-
-func New(ctx context.Context, options JiraOptions) (*Jira, error) {
-	if (options.BasicAuth == nil && options.OAuth == nil) || (options.BasicAuth != nil && options.OAuth != nil) {
-		return nil, errors.New("jira-connector: either BasicAuth or OAuth must be provided")
+type (
+	Jira struct {
+		client *jira.Client
 	}
 
-	if options.BasicAuth != nil {
-		return newBasicAuthJira(ctx, options)
+	JiraBuilder interface {
+		New() (*Jira, error)
 	}
 
-	return newOAuthJira(ctx, options)
-}
+	JiraOptions struct {
+		Url string
+	}
 
-func newOAuthJira(ctx context.Context, options JiraOptions) (*Jira, error) {
-	return &Jira{}, nil
-}
+	JiraBasicAuthBuilder struct {
+		Base *JiraOptions
 
-func newBasicAuthJira(ctx context.Context, options JiraOptions) (*Jira, error) {
-	return &Jira{}, nil
+		Username string
+		ApiToken string
+	}
+)
+
+func (b *JiraBasicAuthBuilder) New() (*Jira, error) {
+	transport := jira.BasicAuthTransport{
+		Username: b.Username,
+		APIToken: b.ApiToken,
+	}
+
+	client, err := jira.NewClient(b.Base.Url, transport.Client())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Jira{
+		client: client,
+	}, nil
 }

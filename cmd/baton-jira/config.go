a85332ace2698a3f3920d8084daf78f5a9941250
Scott Mitchell <scottmitchell2014@gmail.com>
2024-07-25 12:44:57 -0700

    remove env var names

--- cmd/baton-jira/config.go
+++ cmd/baton-jira/config.go
@@ -5,9 +5,9 @@ import (
 )
 
 var (
-	jiraUrlField  = field.StringField("jira-url", field.WithRequired(true), field.WithDescription("Url to Jira service. ($BATON_JIRA_URL)"))
-	emailField    = field.StringField("jira-email", field.WithRequired(true), field.WithDescription("Email for Jira service. ($BATON_JIRA_EMAIL)"))
-	apiTokenField = field.StringField("jira-api-token", field.WithRequired(true), field.WithDescription("API token for Jira service. ($BATON_JIRA_API_TOKEN)"))
+	jiraUrlField  = field.StringField("jira-url", field.WithRequired(true), field.WithDescription("Url to Jira service."))
+	emailField    = field.StringField("jira-email", field.WithRequired(true), field.WithDescription("Email for Jira service."))
+	apiTokenField = field.StringField("jira-api-token", field.WithRequired(true), field.WithDescription("API token for Jira service."))
 )
 
 var configurationFields = []field.SchemaField{

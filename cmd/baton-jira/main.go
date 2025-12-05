f77b4339593c43dcffb5b01f2f430de1d8c792a2
Lauren Leach <lauren.leach@conductorone.com>
2025-12-05 09:10:17 -0800

    no session

--- cmd/baton-jira/main.go
+++ cmd/baton-jira/main.go
@@ -6,12 +6,11 @@ import (
 	cfg "github.com/conductorone/baton-jira/pkg/config"
 	"github.com/conductorone/baton-jira/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
-	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 )
 
 var version = "dev"
 
 func main() {
 	ctx := context.Background()
-	config.RunConnector(ctx, "baton-jira", version, cfg.Config, connector.New, connectorrunner.WithSessionStoreEnabled())
+	config.RunConnector(ctx, "baton-jira", version, cfg.Config, connector.New)
 }
